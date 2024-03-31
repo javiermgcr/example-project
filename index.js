faf9718dbda69f1e0fba0a1c168637aa0f5fb84d
javier <javiermgcr@hotmail.com>
2024-03-31 17:44:32 +0200

    accept

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 console.log("Hello World!");
 
 console.log("😊")
-<<<<<<< bf30cc94a3299b255d3c9091f42a9d822453127a
 console.log("😊")
 console.log("😊")
 console.log("😊")
 console.log("😊")
-=======
-
-console.log("test")
->>>>>>> log test
