2a5a05d1cf970c00a2b2323ce7a042a0e1759886
javier <javiermgcr@hotmail.com>
2024-03-31 17:42:32 +0200

    log test

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("Hello World!");
 
 console.log("😊")
+<<<<<<< bf30cc94a3299b255d3c9091f42a9d822453127a
 console.log("😊")
 console.log("😊")
 console.log("😊")
 console.log("😊")
+=======
+
+console.log("test")
+>>>>>>> log test
